029708cdeb245afa51e69632db05729292f2b0c2
Will <will-hopkins@hotmail.co.uk>
2021-02-27 19:17:07 +0000

    Added type conversion to set command and more error handling

--- src/main/java/org/netsim/cli/SetCommand.java
+++ src/main/java/org/netsim/cli/SetCommand.java
@@ -1,24 +1,20 @@
 package org.netsim.cli;
 
 import lombok.SneakyThrows;
-import org.jline.utils.AttributedStringBuilder;
-import org.jline.utils.AttributedStyle;
 import org.netsim.models.EmptyModel;
 import org.netsim.models.Model;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 @Command(name = "set",
-         description = "Set model options")
+        description = "Set model options")
 public class SetCommand implements Runnable {
 
-    @Parameters
-    private String option;
-
-    @Parameters
-    private int value;
+    @Parameters(paramLabel = "option=value")
+    private Map<String, String> option;
 
     @SneakyThrows
     @Override
@@ -29,11 +25,50 @@ public class SetCommand implements Runnable {
             CommandShell.printErr("Select a model first");
         }
 
-        for (Field field: clazz.getDeclaredFields()) {
-            if (field.getName().equals(option) && field.isAnnotationPresent(Option.class)) {
-                field.setAccessible(true);
-                field.set(selectedModel, value);
+        option.forEach((k, v) -> {
+            Field field;
+            try {
+                field = clazz.getDeclaredField(k);
+            } catch (NoSuchFieldException e) {
+                CommandShell.printErr("No such field: " + k);
+                return;
             }
-        }
+            if (field.isAnnotationPresent(Option.class)) {
+                field.setAccessible(true);
+                String fieldType = field.getType().getName();
+                try {
+                    switch (fieldType) {
+                        case "boolean":
+                            field.set(selectedModel, Boolean.parseBoolean(v));
+                            break;
+                        case "byte":
+                            field.set(selectedModel, Byte.parseByte(v));
+                            break;
+                        case "double":
+                            field.set(selectedModel, Double.parseDouble(v));
+                            break;
+                        case "float":
+                            field.set(selectedModel, Float.parseFloat(v));
+                            break;
+                        case "int":
+                            field.set(selectedModel, Integer.parseInt(v));
+                            break;
+                        case "long":
+                            field.set(selectedModel, Long.parseLong(v));
+                            break;
+                        case "short":
+                            field.set(selectedModel, Short.parseShort(v));
+                            break;
+                        default:
+                            field.set(selectedModel, v);
+                    }
+                } catch(IllegalArgumentException e) {
+                    CommandShell.printErr("Invalid type. Option has type: " + fieldType);
+                } catch (IllegalAccessException ignored) {}
+
+            } else {
+                CommandShell.printErr("No such field: " + k);
+            }
+        });
     }
 }
