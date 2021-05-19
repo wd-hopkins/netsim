c57d9a07430fef0492c31fb772d06b003b6fd447
William Hopkins <will-hopkins@hotmail.co.uk>
2021-05-19 18:25:20 +0100

    Updated links to collect timing data

--- links/CustomLink.java
+++ links/CustomLink.java
@@ -1,9 +1,15 @@
 import org.netsim.models.Node;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalTime;
+
 import static java.time.temporal.ChronoUnit.MILLIS;
 
 public class CustomLink extends Node {
+    
+    private BufferedWriter bw;
 
     public CustomLink(String name) {
         super(name);
@@ -15,10 +21,20 @@ public class CustomLink extends Node {
     }
 
     @Override
-    public void onReceive(Object message) {
+    public synchronized void onReceive(Object message) {
         LocalTime time = (LocalTime) message;
         LocalTime now = LocalTime.now();
-        System.out.printf("[%s][%s] Transmission time: %s ms\n", now, this.name, MILLIS.between(time, now));
+        long diff = MILLIS.between(time, now);
+        System.out.printf("[%s][%s] Transmission time: %s ms\n", now, this.name, diff);
+        try {
+            bw = new BufferedWriter(new FileWriter("data", true));
+            bw.write(String.valueOf(diff));
+            bw.newLine();
+            bw.close();
+        } catch (IOException ignored) {
+            System.out.println("Failure writing to file.");
+            return;
+        }
         send(LocalTime.now(), "out");
     }
 }
